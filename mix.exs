71c626d9ee3f63276ec89037d70d60ef3f48f670
Weixi Yen <weixiyen@gmail.com>
2017-12-18 18:09:52 -0800

    upgrade to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Triton.Mixfile do
 
   def project do
     [app: :triton,
-     version: "0.0.1",
+     version: "0.1.0",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
