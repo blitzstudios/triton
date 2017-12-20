2662558d9653130c98101974560e8ab97927b8fc
Weixi Yen <weixiyen@gmail.com>
2017-12-20 00:36:39 -0800

    0.1.3

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v 0.1.3
+
+Fix DateTimes and change how streams work
+
 ## v 0.1.1
 
 Remove MIX.env from codebase
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Triton.Mixfile do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.3"
   @url "https://github.com/blitzstudios/triton"
   @maintainers ["Weixi Yen"]
 
