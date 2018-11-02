c2a5298a5c8a48836ba92dc2a82061de430d1e0e
Grant McLendon <grant.mclendon@gmail.com>
2018-11-02 12:09:17 -0400

    Refactor CQL.Delete & CQL.Insert to spend less time coercing strings

--- lib/triton/cql/select.ex
+++ lib/triton/cql/select.ex
@@ -3,27 +3,41 @@ defmodule Triton.CQL.Select do
     schema = query[:__schema__].__fields__
 
     select(query[:select], query[:count], query[:__table__], schema) <>
-    where(query[:where]) <>
-    order_by(query[:order_by] && List.first(query[:order_by])) <>
-    limit(query[:limit]) <>
-    allow_filtering(query[:allow_filtering])
+      where(query[:where]) <>
+      order_by(query[:order_by] && List.first(query[:order_by])) <>
+      limit(query[:limit]) <> allow_filtering(query[:allow_filtering])
   end
 
-  defp select(_, count, table, _) when count === true, do: "SELECT COUNT(*) FROM #{table}"
+  defp select(_, count, table, _) when count == true, do: "SELECT COUNT(*) FROM #{table}"
+
   defp select(fields, _, table, schema) when is_list(fields) do
-    schema_fields = schema |> Enum.map(fn {k, _} -> "#{k}" end)
-    req_fields = fields |> Enum.map(fn k -> "#{k}" end)
-    filtered_fields = MapSet.intersection(MapSet.new(req_fields), MapSet.new(schema_fields)) |> Enum.into([])
-    "SELECT #{Enum.join(filtered_fields, ", ")} FROM #{table}"
+    schema_fields = schema |> Enum.into(MapSet.new(), fn {k, _} -> to_string(k) end)
+    req_fields = fields |> Enum.into(MapSet.new(), &to_string/1)
+    filtered_fields = MapSet.intersection(req_fields, schema_fields) |> Enum.join(", ")
+
+    "SELECT " <> filtered_fields <> " FROM #{table}"
   end
+
   defp select(_, _, table, _), do: "SELECT * FROM #{table}"
 
-  defp where(fragments) when is_list(fragments), do: " WHERE " <> (fragments |> Enum.flat_map(fn fragment -> where_fragment(fragment) end) |> Enum.join(" AND "))
+  defp where(fragments) when is_list(fragments),
+    do:
+      " WHERE " <>
+        (fragments
+         |> Enum.flat_map(fn fragment -> where_fragment(fragment) end)
+         |> Enum.join(" AND "))
+
   defp where(_), do: ""
-  defp where_fragment({k, v}) when is_list(v), do: v |> Enum.map(fn {c, v} -> where_fragment({k, c, v}) end)
-  defp where_fragment({k, v}), do: ["#{k} = #{value(v)}"]
-  defp where_fragment({k, :in, v}), do: "#{k} IN (#{v |> Enum.map(fn v -> value(v) end) |> Enum.join(", ")})"
-  defp where_fragment({k, c, v}), do: "#{k} #{c} #{value(v)}"
+
+  defp where_fragment({k, v}) when is_list(v),
+    do: v |> Enum.map(fn {c, v} -> where_fragment({k, c, v}) end)
+
+  defp where_fragment({k, v}), do: ["#{k} = " <> value(v)]
+
+  defp where_fragment({k, :in, v}),
+    do: "#{k} IN ( " <> Enum.map_join(v, ", ", fn v -> value(v) end) <> ")"
+
+  defp where_fragment({k, c, v}), do: "#{k} #{c} " <> value(v)
 
   defp order_by({field, direction}), do: " ORDER BY #{field} #{direction}"
   defp order_by(_), do: ""
@@ -35,8 +49,7 @@ defmodule Triton.CQL.Select do
   defp allow_filtering(true), do: " ALLOW FILTERING"
   defp allow_filtering(_), do: ""
 
-  defp value(v) when is_binary(v), do: "'#{v}'"
-  defp value(v) when is_boolean(v), do: "#{v}"
+  defp value(v) when is_binary(v), do: "'" <> v <> "'"
   defp value(v) when is_atom(v), do: ":#{v}"
-  defp value(v), do: v
+  defp value(v), do: to_string(v)
 end
