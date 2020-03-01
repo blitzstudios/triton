5f4e4d527ab0d6ce554afd05bb915903f5301ee5
Kenneth Ito <kenneth.ito@gmail.com>
2020-03-01 14:21:19 -0800

    Handle encoding strings with single quotes an dollar signs (#32)

--- lib/triton/cql/insert.ex
+++ lib/triton/cql/insert.ex
@@ -9,21 +9,9 @@ defmodule Triton.CQL.Insert do
   defp insert(fields, table, schema) when is_list(fields), do: "INSERT INTO #{table} (#{field_keys(fields)}) VALUES (#{field_values(fields, schema)})"
   defp field_keys(fields) when is_list(fields), do: fields |> Enum.map(fn {k, _} -> k end) |> Enum.join(", ")
   defp field_values(fields, schema) when is_list(fields), do: fields |> Enum.map(fn {k, v} -> field_value(v, schema[k][:type]) end) |> Enum.join(", ")
-  defp field_value(nil, _), do: "NULL"
-  defp field_value(field, {_, _}), do: field
-  defp field_value(field, _) when is_boolean(field), do: "#{field}"
-  defp field_value(field, _) when is_binary(field), do: binary_value(field)
-  defp field_value(field, _) when is_atom(field), do: ":#{field}"
-  defp field_value(%DateTime{} = d, _), do: DateTime.to_unix(d, :millisecond)
-  defp field_value(field, _), do: field
+  defp field_value(v, {_collection_type, _inner_types}) when not is_nil(v), do: v
+  defp field_value(field, _field_type), do: Triton.CQL.Encode.encode(field)
 
   defp if_not_exists(flag) when flag == true, do: " IF NOT EXISTS"
   defp if_not_exists(_), do: ""
-
-  defp binary_value(v) do
-    cond do
-      String.valid?(v) && String.contains?(v, ["'", "\""]) -> "$$#{v}$$"
-      true -> "'#{v}'"
-    end
-  end
 end
