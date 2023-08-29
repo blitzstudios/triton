dee2999474b6ff87cc4e387fd9efbfa33a2b0bde
Peixian Wang <peixian@malloc.dog>
2023-08-29 14:15:25 -0400

    change to 2.4.2

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Triton.Mixfile do
 
   defp deps do
     [
-      {:xandra, github: "blitzstudios/xandra", tag: "xandra-pool-metrics"},
+      {:xandra, github: "blitzstudios/xandra", tag: "2.4.2-conn-metrics"},
       {:vex, "~> 0.6"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
